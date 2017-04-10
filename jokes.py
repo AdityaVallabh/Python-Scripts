c8409cf2d45424970c8a2b7f28c853ad7ea94aed
AdityaVallabh <adityavallabhganti@gmail.com>
2017-04-10 22:22:28 +0530

    Stores as JSON

--- jokes.py
+++ jokes.py
@@ -1,37 +1,29 @@
 import requests
+import json
+import time
 from bs4 import BeautifulSoup
 
-def scrape():
+def scrape(page):
 
-    url = "http://123hindijokes.com/very-funny-jokes/"
-
+    url = "http://123hindijokes.com/very-funny-jokes/" + str(page)
+    json_list = {}
     code = requests.get(url)
     soup = BeautifulSoup(code.content, "lxml")
-    text = soup.findAll('ul', {'class': 'statusList'})
+    uls = soup.findAll('ul', {'class': 'statusList'})
+    count = 1
+    for ul in uls:
+    	for li in ul.findAll('li'):
+    		json_list[count] = li.get_text()
+    		count += 1
 
-    f = open('jokes.html', 'w')
+    with open('jokes.json','w') as f:
+    	json.dump(json_list, f, ensure_ascii=False, indent=4)
+    print("Scraped Page " + str(page))
 
-    html = """
-<!DOCTYPE html>
-<html>
-<head>
- 	<meta charset="utf-8">
-	<title>Scraped Hindi Jokes</title>
-</head>
-<body>
-<h1>Python Web Scraping</h1>
-<p>Scraped from: <a href="http://123hindijokes.com/very-funny-jokes/">http://123hindijokes.com/very-funny-jokes/</a></p>
-<p>
-    """
-    html += str(text)[1:-1]
-    html += """
-</p>
-</body>
-</html>
-    """
+page = 0
 
-    f.write(html)
-    f.close()
-    print("Success!")   
+while True:
+	scrape(page % 11 + 1)
+	time.sleep(10)
+	page += 1
 
-scrape()
